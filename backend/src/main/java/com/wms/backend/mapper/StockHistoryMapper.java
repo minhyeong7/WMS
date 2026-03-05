9585a30888dce2713d6a827a0c2b6dffa9898b8b
minhyeong7 <163122988+minhyeong7@users.noreply.github.com>
2026-03-05 22:26:41 +0900

    이력 조회 매퍼 추가

--- backend/src/main/java/com/wms/backend/mapper/StockHistoryMapper.java
+++ backend/src/main/java/com/wms/backend/mapper/StockHistoryMapper.java
@@ -2,10 +2,27 @@ package com.wms.backend.mapper;
 
 import com.wms.backend.entity.StockHistory;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
 
 @Mapper
 public interface StockHistoryMapper {
 
     // 반출,반입 이력
     void insert(StockHistory stockHistory);
+
+    // 이력 조회
+    List<StockHistory> select(
+            @Param("filter") String filter, // 반입 or 반출 필터링
+            @Param("sortColumn") String sortColumn, // 정렬 기준
+            @Param("sortDir") String sortDir, // 정렬 방향
+            @Param("offset")  int offset ,// 페이징 시작 위치
+            @Param("size")    int size // 데이터 개수
+    );
+
+    // 이력 조회 개수
+    int countHistory(
+            @Param("filter") String filter
+    );
+
 }
