35fb456af3b2a587569fd3793c694fbcdeee4378
minhyeong7 <163122988+minhyeong7@users.noreply.github.com>
2026-03-07 22:49:00 +0900

    stockAPi 호출 구현

---
+++ frontend/src/api/stockApi.ts
@@ -0,0 +1,32 @@
+import type { StockHistoryRequest } from "../types/StockHistoryRequest";
+import { api } from "./axios";
+
+
+
+// 반입
+export const stockIn = async(id:number,data:StockHistoryRequest)  => {
+    const res = await api.post(`/${id}/in`,data);
+
+    console.log("반입:",res)
+
+
+    return res.data;
+}
+
+// 반출
+export const stockOut = async(id:number,data:StockHistoryRequest) => {
+     const res = await api.post(`/${id}/out`,data);
+
+     console.log("반출:",res)
+
+    return res.data;
+}
+
+// 이력 조회
+export const getHistory =async (params = {}) => {
+    const res = await api.get("/history", {params});
+
+    console.log("이력 조회",res)
+
+    return res.data;
+}
---
+++ frontend/src/types/StockHistoryRequest.ts
@@ -0,0 +1,3 @@
+export interface StockHistoryRequest{
+    movementQuantity : number;
+}
